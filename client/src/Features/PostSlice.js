627c00ad260950e74c823e51d6b4f17497c00730
SHEIKHA66 <66S1928@utas.edu.om>
2024-12-09 14:34:54 +0400

    env update

--- client/src/Features/PostSlice.js
+++ client/src/Features/PostSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import * as ENV from "../config";
 
 const initialState = {
   posts: [],
@@ -44,7 +45,8 @@ const postSlice = createSlice({
 
 export const savePost = createAsyncThunk("posts/savePost", async (postData) => {
   try {
-    const response = await axios.post("http://localhost:3001/savePost", {
+    // const response = await axios.post("http://localhost:3001/savePost", {
+    const response = await axios.post(`${ENV.SERVER_URL}/savePost`, {
       postMsg: postData.postMsg,
       email: postData.email,
     });
@@ -57,7 +59,9 @@ export const savePost = createAsyncThunk("posts/savePost", async (postData) => {
 
 export const getPosts = createAsyncThunk("post/getPosts", async () => {
   try {
-    const response = await axios.get("http://localhost:3001/getPosts");
+    //  const response = await axios.get("http://localhost:3001/getPosts");
+
+    const response = await axios.get(`${ENV.SERVER_URL}/getPosts`);
     return response.data.posts;
     console.log(response);
   } catch (error) {
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import UserModel from "./Models/UserModel.js";
 import bcrypt from "bcrypt";
 import PostModel from "./Models/Posts.js";
+import * as ENV from "./config.js";
 
 const app = express();
 
@@ -12,8 +13,9 @@ app.use(express.json());
 app.use(cors());
 
 //db connection
-const connectString =
-  "mongodb+srv://user123:user123@postitcluster.rwtav.mongodb.net/postITDb?retryWrites=true&w=majority&appName=PostITCluster";
+//const connectString = "mongodb+srv://user123:user123@postitcluster.rwtav.mongodb.net/postITDb?retryWrites=true&w=majority&appName=PostITCluster";
+
+const connectString = `mongodb+srv://${ENV.DB_USER}:${ENV.DB_PASSWORD}@${ENV.DB_CLUSTER}/${ENV.DB_NAME}?retryWrites=true&w=majority&appName=PostITCluster`;
 
 //mongodb+srv://sem120242025:<db_password>@postitcluster.gsgf6.mongodb.net/?retryWrites=true&w=majority&appName=PostITCluster
 //mongodb+srv://sem120242025:<db_password>@postitcluster.gsgf6.mongodb.net/?retryWrites=true&w=majority&appName=PostITCluster
@@ -97,6 +99,8 @@ app.get("/getPosts", async (req, res) => {
   }
 });
 
-app.listen(3001, () => {
-  console.log("You are connected!");
+const port = ENV.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`You are connected at port: ${port}`);
 });
