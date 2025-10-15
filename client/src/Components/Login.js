3858e918f2b1841f4e6f416df0c292ac3566e532
SHEIKHA66 <66S1928@utas.edu.om>
2025-10-15 11:16:19 +0400

    Update Login.js

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -56,43 +56,51 @@ const Login = () => {
           <Row>
             <Col>
     
+<Col md={3}>
+              <FormGroup>
+                <Label for="eMail">Email</Label>
+                <Input
+                  id="eMail"
+                  name="eMail"
+                  placeholder="Enter email..."
+                  type="email"
+                  onChange={(e) => setemail(e.target.value)}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={3}>
+              <FormGroup>
+                <Label for="password">Password</Label>
+                <Input
+                  id="password"
+                  name="password"
+                  placeholder="Enter password..."
+                  type="password"
+                  onChange={(e) => setpassword(e.target.value)}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col md={3}>
+              <Button
+                color="primary"
+                className="button"
+                onClick={() => handleLogin()}
+              >
+                Login
+              </Button>
+              <p className="smalltext">
+                No Account? <Link to="/register">Sign Up</Link>
+              </p>
+            </Col>
 
 
-<table>
-    
-    <thead></thead>
 
-    <tbody>
-    
-  
-
-    </tbody>
-      <td>
- <p>Imports</p>
- <p>Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force </p>
-  <p>npx create-react-app . </p>
- <p>npm install</p>
- <p> npm start</p>
- <p>npm install reactstrap</p>
- <p>npm install  bootstrap</p>
- <p>npm install yup</p>
- <p>npm install react-hook-form</p>
- <p>npm install @hookform/resolvers</p>
- <p>npm install @reduxjs/toolkit react-redux</p>
-
-    
------- Home----------
- <p> import logo from "../Images/logo-t.png";</p>
-<p>import Posts from "./Posts";</p>
-<p>import SharePost from "./SharePost";</p>
-<p>import User from "./User";</p>
-<p>import { Container, Row, Col } from "reactstrap"; //import the Reactstrap Components</p>
-
- 
-
-
-    </td>
-    </table>
     </Col>
           </Row>
         </Form>
